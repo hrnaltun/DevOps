5cd541f433e3f86870543c1f89c3a8cdd715a25c
Harun Altun <80642843+hrnaltun@users.noreply.github.com>
2024-02-01 15:52:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,32 +7,32 @@ api = Api(app)
 
 class Apart(Resource):
     def get(self):
-        data = pd.read_csv('apart.csv')
+        data = pd.read_csv('apart.csv', encoding='latin1')  # Specify encoding here
         data = data.to_dict('records')
-        return {'data' : data}, 200
+        return {'data': data}, 200
 
     def post(self):
         isim = request.args['isim']
         kat = request.args['kat']
         odasayısı = request.args['odasayısı']
-        m2= request.args['m2']
+        m2 = request.args['m2']
         req_data = pd.DataFrame({'isim': [isim],
-				  'kat': [kat],
-                                  'odasayısı': [odasayısı],
-				  'm2':[m2]})
-        data = pd.read_csv('apart.csv')
+                                 'kat': [kat],
+                                 'odasayısı': [odasayısı],
+                                 'm2': [m2]})
+        data = pd.read_csv('apart.csv', encoding='latin1')  # Specify encoding here
         data = data.append(req_data, ignore_index=True)
-        data.to_csv('apart.csv', index=False)
-        return ({'message': 'Record successfully added.'}, 200)
+        data.to_csv('apart.csv', index=False, encoding='latin1')  # Specify encoding here
+        return {'message': 'Record successfully added.'}, 200
 
 class Name(Resource):
     def get(self):
-        data = pd.read_csv('apart.csv',usecols=[0])
+        data = pd.read_csv('apart.csv', usecols=[0], encoding='latin1')  # Specify encoding here
         data = data.to_dict('records')
-        return {'data' : data}, 200
+        return {'data': data}, 200
 
 api.add_resource(Name, '/apartname')
 api.add_resource(Apart, '/aparts')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",)
+    app.run(host="0.0.0.0")
